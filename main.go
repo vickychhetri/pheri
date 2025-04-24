4baf6334a5bb0c9cb519e18159d6f61343934479
Vicky Chhetri <specialpower321@gmail.com>
2025-04-25 00:08:32 +0530

    command line flags / values

--- main.go
+++ main.go
@@ -2,14 +2,30 @@
 package main
 
 import (
+	"flag"
 	"mysql-tui/ui"
 
 	"github.com/rivo/tview"
 )
 
 func main() {
+
+	user := flag.String("u", "", "Username")
+	pass := flag.String("p", "", "Password")
+	host := flag.String("host", "localhost", "Hostname")
+	port := flag.String("port", "3306", "Port number") // Example default for PostgreSQL
+
+	flag.Parse()
+
+	var password string
+	if *pass != "" {
+		password = *pass
+	} else {
+		password = ""
+	}
+
 	app := tview.NewApplication()
-	ui.ShowConnectionForm(app)
+	ui.ShowConnectionForm(app, *user, password, *host, *port)
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
--- ui/connect.go
+++ ui/connect.go
@@ -3,6 +3,7 @@ package ui
 
 import (
 	"database/sql"
+	"log"
 	"mysql-tui/dbs"
 
 	"github.com/rivo/tview"
@@ -10,41 +11,51 @@ import (
 
 var user, pass, host, port string
 
-func ShowConnectionForm(app *tview.Application) {
+func ShowConnectionForm(app *tview.Application, user, pass, host, port string) {
 	var form *tview.Form
 
-	form = tview.NewForm().
-		AddInputField("Host", "127.0.0.1", 20, nil, nil).
-		AddInputField("Port", "3306", 6, nil, nil).
-		AddInputField("User", "root", 20, nil, nil).
-		AddPasswordField("Password", "", 20, '*', nil).
-		AddButton("Connect", func() {
-			host = form.GetFormItemByLabel("Host").(*tview.InputField).GetText()
-			port = form.GetFormItemByLabel("Port").(*tview.InputField).GetText()
-			user = form.GetFormItemByLabel("User").(*tview.InputField).GetText()
-			pass = form.GetFormItemByLabel("Password").(*tview.InputField).GetText()
-
-			conn, err := dbs.Connect(user, pass, host, port)
-			if err != nil {
-				modal := tview.NewModal().
-					SetText("Connection failed: " + err.Error()).
-					AddButtons([]string{"OK"}).
-					SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-						app.SetRoot(form, true)
-					})
-				app.SetRoot(modal, true)
-				return
-			}
-
-			ShowDatabaseList(app, conn)
-		}).
-		AddButton("Quit", func() {
+	if user != "" && pass != "" && host != "" && port != "" {
+		conn, err := dbs.Connect(user, pass, host, port)
+		if err != nil {
+			log.Printf("Error in db Connection: %v", err)
 			app.Stop()
-		})
+		}
+		ShowDatabaseList(app, conn)
 
-	form.SetBorder(true).SetTitle("MySQL Connection").SetTitleAlign(tview.AlignCenter)
-	layout := CreateLayoutWithFooter(form)
-	app.SetRoot(layout, true).SetFocus(form)
+	} else {
+		form = tview.NewForm().
+			AddInputField("Host", "127.0.0.1", 20, nil, nil).
+			AddInputField("Port", "3306", 6, nil, nil).
+			AddInputField("User", "root", 20, nil, nil).
+			AddPasswordField("Password", "", 20, '*', nil).
+			AddButton("Connect", func() {
+				host = form.GetFormItemByLabel("Host").(*tview.InputField).GetText()
+				port = form.GetFormItemByLabel("Port").(*tview.InputField).GetText()
+				user = form.GetFormItemByLabel("User").(*tview.InputField).GetText()
+				pass = form.GetFormItemByLabel("Password").(*tview.InputField).GetText()
+
+				conn, err := dbs.Connect(user, pass, host, port)
+				if err != nil {
+					modal := tview.NewModal().
+						SetText("Connection failed: " + err.Error()).
+						AddButtons([]string{"OK"}).
+						SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+							app.SetRoot(form, true)
+						})
+					app.SetRoot(modal, true)
+					return
+				}
+
+				ShowDatabaseList(app, conn)
+			}).
+			AddButton("Quit", func() {
+				app.Stop()
+			})
+
+		form.SetBorder(true).SetTitle("MySQL Connection").SetTitleAlign(tview.AlignCenter)
+		layout := CreateLayoutWithFooter(form)
+		app.SetRoot(layout, true).SetFocus(form)
+	}
 
 }
 
@@ -69,7 +80,7 @@ func ShowDatabaseList(app *tview.Application, db *sql.DB) {
 	}
 
 	list.AddItem("Back", "Return to connection screen", 'b', func() {
-		ShowConnectionForm(app)
+		ShowConnectionForm(app, user, pass, host, port)
 	})
 
 	layout := CreateLayoutWithFooter(list)
